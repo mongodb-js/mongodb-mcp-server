8ae3d3d39eddda9dc7b779c9f65dbdffd3793d03
Himanshu Singh <himanshu.singhs@outlook.in>
2025-07-01 14:05:51 +0200

    chore: use ListCollectionsTool response creators in tests

--- tests/accuracy/list-collections.test.ts
+++ tests/accuracy/list-collections.test.ts
@@ -1,25 +1,16 @@
 import { describeAccuracyTests } from "./sdk/describe-accuracy-tests.js";
 import { getAvailableModels } from "./sdk/models.js";
 import { AccuracyTestConfig } from "./sdk/describe-accuracy-tests.js";
+import { listCollectionsResponse } from "../../src/tools/mongodb/metadata/listCollections.js";
+import { listDatabasesResponse } from "../../src/tools/mongodb/metadata/listDatabases.js";
 
-function describeListCollectionsAccuracyTests(prompt: string): AccuracyTestConfig {
+function callsListCollections(prompt: string): AccuracyTestConfig {
     return {
         injectConnectedAssumption: true,
         prompt: prompt,
         mockedTools: {
             "list-collections": function listCollections() {
-                return {
-                    content: [
-                        {
-                            type: "text",
-                            text: "Name: coll1",
-                        },
-                        {
-                            type: "text",
-                            text: "Name: coll1",
-                        },
-                    ],
-                };
+                return listCollectionsResponse("db1", ["coll1", "coll2"]);
             },
         },
         expectedToolCalls: [
@@ -31,8 +22,47 @@ function describeListCollectionsAccuracyTests(prompt: string): AccuracyTestConfi
     };
 }
 
+function callsListDatabasesAndListCollections(prompt: string): AccuracyTestConfig {
+    return {
+        injectConnectedAssumption: true,
+        prompt: prompt,
+        mockedTools: {
+            "list-collections": function listCollections() {
+                return listCollectionsResponse("db1", ["coll1", "coll2"]);
+            },
+            "list-databases": function listDatabases() {
+                return listDatabasesResponse([
+                    {
+                        name: "db1",
+                        sizeOnDisk: "1024",
+                    },
+                    {
+                        name: "db2",
+                        sizeOnDisk: "2048",
+                    },
+                ]);
+            },
+        },
+        expectedToolCalls: [
+            {
+                toolName: "list-databases",
+                parameters: {},
+            },
+            {
+                toolName: "list-collections",
+                parameters: { database: "db1" },
+            },
+            {
+                toolName: "list-collections",
+                parameters: { database: "db2" },
+            },
+        ],
+    };
+}
+
 describeAccuracyTests("list-collections", getAvailableModels(), [
-    describeListCollectionsAccuracyTests("How many collections do I have in database db1?"),
-    describeListCollectionsAccuracyTests("List all the collections in my MongoDB database db1."),
-    describeListCollectionsAccuracyTests("Is there a coll1 collection in my MongoDB database db1?"),
+    callsListCollections("How many collections do I have in database db1?"),
+    callsListCollections("List all the collections in my MongoDB database db1."),
+    callsListCollections("Is there a coll1 collection in my MongoDB database db1?"),
+    callsListDatabasesAndListCollections("List all the collections that I have in total on my cluster?"),
 ]);
