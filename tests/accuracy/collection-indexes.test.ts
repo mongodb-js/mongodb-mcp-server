77152c71aa5e92809fd9baff9b39f44032e2b1ff
Himanshu Singh <himanshu.singhs@outlook.in>
2025-07-01 14:30:50 +0200

    chore: tests for collection-indexes tool

---
+++ tests/accuracy/collection-indexes.test.ts
@@ -0,0 +1,42 @@
+import { describeAccuracyTests } from "./sdk/describe-accuracy-tests.js";
+import { getAvailableModels } from "./sdk/models.js";
+import { AccuracyTestConfig } from "./sdk/describe-accuracy-tests.js";
+import { collectionIndexesResponse } from "../../src/tools/mongodb/read/collectionIndexes.js";
+
+function callsCollectionIndexes(prompt: string): AccuracyTestConfig {
+    return {
+        injectConnectedAssumption: true,
+        prompt: prompt,
+        mockedTools: {
+            "collection-indexes": function collectionIndexes() {
+                return collectionIndexesResponse({
+                    database: "db1",
+                    collection: "coll1",
+                    indexes: [
+                        {
+                            name: "year",
+                            key: JSON.stringify({ _id: 1 }),
+                        },
+                    ],
+                });
+            },
+        },
+        expectedToolCalls: [
+            {
+                toolName: "collection-indexes",
+                parameters: {
+                    database: "db1",
+                    collection: "coll1",
+                },
+            },
+        ],
+    };
+}
+
+describeAccuracyTests("collection-indexes", getAvailableModels(), [
+    callsCollectionIndexes("How many indexes do I have in 'db1.coll1' namespace?"),
+    callsCollectionIndexes("List all the indexes in coll1 collection in db1 database"),
+    callsCollectionIndexes(
+        `Will this query: ${JSON.stringify({ year: 1994 })} on the namespace 'db1.coll1' be a collection scan?`
+    ),
+]);
