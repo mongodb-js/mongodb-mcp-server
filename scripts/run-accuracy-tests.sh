b34f6bc9ca5d237307b2300d5b2a27e5665df98d
Himanshu Singh <himanshu.singhs@outlook.in>
2025-07-08 21:14:01 +0200

    chore: adds accuracyRunStatus to snapshot entries
    The new field `accuracyRunStatus` is supposed to help guard against
    cases where jest might fail in between, maybe due to LLM rate limit
    errors or something else, and we then have a partially saved state of an
    accuracy run. With the new field `accuracyRunStatus` we should be able
    to safely look for last runs where `accuracyRunStatus` is done and have
    complete state of accuracy snapshot.

--- scripts/run-accuracy-tests.sh
+++ scripts/run-accuracy-tests.sh
@@ -2,6 +2,27 @@
 # Variables necessary for the accuracy test runs
 export MDB_ACCURACY_RUN_ID=$(npx uuid v4)
 
+# For providing access tokens for different LLM providers
+# export MDB_OPEN_AI_API_KEY=""
+# export MDB_GEMINI_API_KEY=""
+# export MDB_AZURE_OPEN_AI_API_KEY=""
+# export MDB_AZURE_OPEN_AI_API_URL=""
+
+# For providing a mongodb based storage to store accuracy snapshots
+# export MDB_ACCURACY_MDB_URL=""
+# export MDB_ACCURACY_MDB_DB=""
+# export MDB_ACCURACY_MDB_COLLECTION=""
+
+# By default we run all the tests under tests/accuracy folder unless a path is
+# specified in the command line. Such as:
+# npm run test:accuracy -- tests/accuracy/some-test.test.ts
 TEST_PATH_PATTERN="${1:-tests/accuracy}"
 shift || true
 node --experimental-vm-modules node_modules/jest/bin/jest.js --testPathPattern "$TEST_PATH_PATTERN" "$@"
+
+# Each test run submits an accuracy snapshot entry for each prompt with the
+# accuracyRunStatus: "in-progress". When all the tests are done and jest exits
+# with an exit code of 0, we can safely mark accuracy run as finished.
+if [ $? -eq 0 ]; then
+  npx tsx scripts/mark-accuracy-run-finished.ts
+fi
