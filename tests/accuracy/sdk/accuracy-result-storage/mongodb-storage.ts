acba3b41a5715703fb4b928cd78a7ea1feae2731
Himanshu Singh <himanshu.singhs@outlook.in>
2025-07-15 12:43:22 +0200

    chore: use $literal instead of serializing the tool calls

--- tests/accuracy/sdk/accuracy-result-storage/mongodb-storage.ts
+++ tests/accuracy/sdk/accuracy-result-storage/mongodb-storage.ts
@@ -13,30 +13,13 @@ import {
 // Omitting these as they might contain large chunk of texts
 const OMITTED_MODEL_RESPONSE_FIELDS: (keyof ModelResponse)[] = ["messages", "text"];
 
-// The LLMToolCalls and ExpectedToolCalls are expected to have mongodb operators
-// nested in the objects. This interferes with the update operation that we do
-// on the accuracy result document to save the model responses which is why we
-// serialize them before saving and deserialize them on fetch.
-type SavedAccuracyResult = Omit<AccuracyResult, "promptResults"> & {
-    promptResults: SavedPromptResult[];
-};
-
-type SavedPromptResult = Omit<PromptResult, "expectedToolCalls" | "modelResponses"> & {
-    expectedToolCalls: string;
-    modelResponses: SavedModelResponse[];
-};
-
-type SavedModelResponse = Omit<ModelResponse, "llmToolCalls"> & {
-    llmToolCalls: string;
-};
-
 export class MongoDBBasedResultStorage implements AccuracyResultStorage {
     private client: MongoClient;
-    private resultCollection: Collection<SavedAccuracyResult>;
+    private resultCollection: Collection<AccuracyResult>;
 
     constructor(connectionString: string, database: string, collection: string) {
         this.client = new MongoClient(connectionString);
-        this.resultCollection = this.client.db(database).collection<SavedAccuracyResult>(collection);
+        this.resultCollection = this.client.db(database).collection<AccuracyResult>(collection);
     }
 
     async getAccuracyResult(commitSHA: string, runId?: string): Promise<AccuracyResult | null> {
@@ -48,13 +31,11 @@ export class MongoDBBasedResultStorage implements AccuracyResultStorage {
               // particular commit.
               { commitSHA, runStatus: AccuracyRunStatus.Done };
 
-        const result = await this.resultCollection.findOne(filters, {
+        return await this.resultCollection.findOne(filters, {
             sort: {
                 createdOn: -1,
             },
         });
-
-        return result ? this.deserializeSavedResult(result) : result;
     }
 
     async updateRunStatus(commitSHA: string, runId: string, status: AccuracyRunStatuses): Promise<void> {
@@ -81,10 +62,8 @@ export class MongoDBBasedResultStorage implements AccuracyResultStorage {
         expectedToolCalls: ExpectedToolCall[];
         modelResponse: ModelResponse;
     }): Promise<void> {
-        const expectedToolCallsToSave = JSON.stringify(expectedToolCalls);
-        const modelResponseToSave: SavedModelResponse = {
+        const modelResponseToSave: ModelResponse = {
             ...modelResponse,
-            llmToolCalls: JSON.stringify(modelResponse.llmToolCalls),
         };
 
         for (const field of OMITTED_MODEL_RESPONSE_FIELDS) {
@@ -122,9 +101,11 @@ export class MongoDBBasedResultStorage implements AccuracyResultStorage {
                                                 "$promptResults",
                                                 [
                                                     {
-                                                        prompt,
-                                                        expectedToolCalls: expectedToolCallsToSave,
-                                                        modelResponses: [modelResponseToSave],
+                                                        $literal: {
+                                                            prompt,
+                                                            expectedToolCalls,
+                                                            modelResponses: [modelResponseToSave],
+                                                        },
                                                     },
                                                 ],
                                             ],
@@ -138,11 +119,11 @@ export class MongoDBBasedResultStorage implements AccuracyResultStorage {
                                                         { $eq: ["$$promptResult.prompt", prompt] },
                                                         {
                                                             prompt: "$$promptResult.prompt",
-                                                            expectedToolCalls: expectedToolCallsToSave,
+                                                            expectedToolCalls,
                                                             modelResponses: {
                                                                 $concatArrays: [
                                                                     "$$promptResult.modelResponses",
-                                                                    [modelResponseToSave],
+                                                                    [{ $literal: modelResponseToSave }],
                                                                 ],
                                                             },
                                                         },
@@ -160,24 +141,6 @@ export class MongoDBBasedResultStorage implements AccuracyResultStorage {
             ],
             { upsert: true }
         );
-    }
-
-    private deserializeSavedResult(result: SavedAccuracyResult): AccuracyResult {
-        return {
-            ...result,
-            promptResults: result.promptResults.map<PromptResult>((result) => {
-                return {
-                    ...result,
-                    expectedToolCalls: JSON.parse(result.expectedToolCalls) as ExpectedToolCall[],
-                    modelResponses: result.modelResponses.map<ModelResponse>((response) => {
-                        return {
-                            ...response,
-                            llmToolCalls: JSON.parse(response.llmToolCalls) as LLMToolCall[],
-                        };
-                    }),
-                };
-            }),
-        };
     }
 
     async close(): Promise<void> {
