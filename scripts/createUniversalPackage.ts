6a70bbe02b97f9165d24ff53a69c44bbd7a3ad67
gagik <me@gagik.co>
2025-07-16 10:14:18 +0200

    fix: add backwards compatibility

--- scripts/createUniversalPackage.ts
+++ scripts/createUniversalPackage.ts
@@ -5,6 +5,10 @@ import { resolve } from "path";
 
 const distDir = resolve("dist");
 
+/**
+ * Node uses the package.json to know whether files with a .js extensions
+ * should be interpreted as CommonJS or ESM.
+ */
 // ESM package.json
 const esmPath = resolve(distDir, "esm", "package.json");
 mkdirSync(resolve(distDir, "esm"), { recursive: true });
@@ -14,3 +18,8 @@ writeFileSync(esmPath, JSON.stringify({ type: "module" }));
 const cjsPath = resolve(distDir, "cjs", "package.json");
 mkdirSync(resolve(distDir, "cjs"), { recursive: true });
 writeFileSync(cjsPath, JSON.stringify({ type: "commonjs" }));
+
+// Create a dist/index.js file that imports the ESM index.js file
+// To minimize breaking changes from pre-universal package time.
+const indexPath = resolve(distDir, "index.js");
+writeFileSync(indexPath, `import "./esm/index.js";`);
