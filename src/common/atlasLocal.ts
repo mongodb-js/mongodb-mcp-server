3e46dbb6b8dee3b515cb1c6c7bc92c4a8b51964c
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2025-10-21 08:13:09 +0100

    adressed pr comments in atlasLocal.ts

--- src/common/atlasLocal.ts
+++ src/common/atlasLocal.ts
@@ -6,16 +6,12 @@ export const defaultCreateAtlasLocalClient: AtlasLocalClientFactoryFn = async ()
     try {
         // Import Atlas Local client asyncronously
         // This will fail on unsupported platforms
-        // also measure the time it takes to import the client
         const { Client: AtlasLocalClient } = await import("@mongodb-js/atlas-local");
 
         try {
             // Connect to Atlas Local client
             // This will fail if docker is not running
-            const client = AtlasLocalClient.connect();
-
-            // Set Atlas Local client
-            return client;
+            return AtlasLocalClient.connect();
         } catch (dockerError) {
             console.warn(
                 "Failed to connect to Atlas Local client (Docker not available or not running), atlas-local tools will be disabled (error: ",
