dcfcad00a3a8814305c217c189be8b24cec2ce54
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2025-10-29 12:59:32 +0000

    update tests

--- src/tools/atlas/connect/connectCluster.ts
+++ src/tools/atlas/connect/connectCluster.ts
@@ -22,9 +22,6 @@ function sleep(ms: number): Promise<void> {
 export const ConnectClusterArgs = {
     projectId: AtlasArgs.projectId().describe("Atlas project ID"),
     clusterName: AtlasArgs.clusterName().describe("Atlas cluster name"),
-    connectionType: AtlasArgs.connectionType().describe(
-        "Type of connection (standard, private, or privateEndpoint) to an Atlas cluster"
-    ),
 };
 
 export class ConnectClusterTool extends AtlasToolBase {
