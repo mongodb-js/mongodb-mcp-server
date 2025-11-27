776c8e3dd8f34afcae0e05a7ddb761cfb75c1217
Himanshu Singh <himanshu.singhs@outlook.in>
2025-11-27 13:41:18 +0100

    chore: updated exports for library

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,7 @@
 export { Server, type ServerOptions } from "./server.js";
 export { Session, type SessionOptions } from "./common/session.js";
-export { type UserConfig } from "./common/config/userConfig.js";
+export { type UserConfig, UserConfigSchema } from "./common/config/userConfig.js";
+export { createUserConfig as parseCliArgumentsAsUserConfig } from "./common/config/createUserConfig.js";
 export { LoggerBase, type LogPayload, type LoggerType, type LogLevel } from "./common/logger.js";
 export { StreamableHttpRunner } from "./transports/streamableHttp.js";
 export { StdioRunner } from "./transports/stdio.js";
@@ -8,19 +9,21 @@ export { TransportRunnerBase, type TransportRunnerConfig } from "./transports/ba
 export {
     ConnectionManager,
     ConnectionStateConnected,
+    createMCPConnectionManager,
     type AnyConnectionState,
     type ConnectionState,
     type ConnectionStateDisconnected,
     type ConnectionStateErrored,
     type ConnectionManagerFactoryFn,
 } from "./common/connectionManager.js";
-export type {
-    ConnectionErrorHandler,
-    ConnectionErrorHandled,
-    ConnectionErrorUnhandled,
-    ConnectionErrorHandlerContext,
+export {
+    connectionErrorHandler,
+    type ConnectionErrorHandler,
+    type ConnectionErrorHandled,
+    type ConnectionErrorUnhandled,
+    type ConnectionErrorHandlerContext,
 } from "./common/connectionErrorHandler.js";
-export { ErrorCodes } from "./common/errors.js";
+export { ErrorCodes, MongoDBError } from "./common/errors.js";
 export { Telemetry } from "./telemetry/telemetry.js";
 export { Keychain, registerGlobalSecretToRedact } from "./common/keychain.js";
 export type { Secret } from "./common/keychain.js";
