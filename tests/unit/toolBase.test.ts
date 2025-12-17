226a0ba071873bea4805079e5b2c8d13ebb28652
Terrence Keane <terrence.keane@mongodb.com>
2025-12-17 11:28:00 -0500

    fix: copilot feedback

--- tests/unit/toolBase.test.ts
+++ tests/unit/toolBase.test.ts
@@ -16,11 +16,7 @@ import type { TelemetryToolMetadata, ToolEvent } from "../../src/telemetry/types
 import type { PreviewFeature } from "../../src/common/schemas.js";
 import { UIRegistry } from "../../src/ui/registry/index.js";
 import { TRANSPORT_PAYLOAD_LIMITS } from "../../src/transports/constants.js";
-
-function expectDefined<T>(arg: T): asserts arg is Exclude<T, undefined | null> {
-    expect(arg).toBeDefined();
-    expect(arg).not.toBeNull();
-}
+import { expectDefined } from "../integration/helpers.js";
 
 describe("ToolBase", () => {
     let mockSession: Session;
