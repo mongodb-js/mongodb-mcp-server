b8cac268f3e2ba0b2960a5dc81b2e6e5eecc9dcd
gagik <me@gagik.co>
2026-01-15 14:55:09 +0100

    chore: remove redundant setup

--- tests/browser/utils/setup.ts
+++
@@ -1,5 +0,0 @@
-// Browser test setup - inject Node.js globals that are expected by MongoDB driver
-import { Buffer } from "buffer";
-
-// Make Buffer available globally
-globalThis.Buffer = Buffer;
