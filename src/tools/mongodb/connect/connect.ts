9b61d0c6254fc248c7c061702d2771b948db84a7
Bianca Lisle <bianca.vianadeaguiar@mongodb.com>
2026-01-19 11:18:31 +0000

    lint

--- src/tools/mongodb/connect/connect.ts
+++ src/tools/mongodb/connect/connect.ts
@@ -3,8 +3,6 @@ import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
 import { MongoDBToolBase } from "../mongodbTool.js";
 import type { ToolArgs, OperationType, ToolConstructorParams } from "../../tool.js";
 import type { Server } from "../../../server.js";
-import { getHostType } from "../../../common/connectionInfo.js";
-import type { ConnectionMetadata } from "../../../telemetry/types.js";
 export class ConnectTool extends MongoDBToolBase {
     public override name = "connect";
     public override description =
