ca3708f1f95c26be646ce512e2e8e54fadef6868
Gagik Amaryan <gagik.amaryan@mongodb.com>
2026-03-26 17:55:10 +0100

    chore: use error_type instead of reason

--- src/server.ts
+++ src/server.ts
@@ -300,6 +300,7 @@ export class Server<
             event.properties.runtime_duration_ms = Date.now() - this.startTime;
             if (error) {
                 event.properties.result = "failure";
+                event.properties.error_type = error.name;
             }
         }
 
