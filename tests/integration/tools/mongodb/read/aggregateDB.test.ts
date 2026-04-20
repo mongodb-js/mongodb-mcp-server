41e7290748a2deda6f369c057d703b07ddcf912c
Paula Stachova <paula.stachova@mongodb.com>
2026-04-20 16:10:37 +0200

    add more time

--- tests/integration/tools/mongodb/read/aggregateDB.test.ts
+++ tests/integration/tools/mongodb/read/aggregateDB.test.ts
@@ -438,7 +438,7 @@ describeWithMongoDB(
 
             const { result, error, executionTime } = await aggregatePromise;
 
-            expect(executionTime).toBeLessThan(25); // Ensure it aborted quickly
+            expect(executionTime).toBeLessThan(100); // Ensure it aborted quickly
             expect(result).toBeUndefined();
             expectDefined(error);
             expect(error.message).toContain("This operation was aborted");
