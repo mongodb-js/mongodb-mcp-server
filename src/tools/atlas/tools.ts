3c99d39f4e89017a366f797878406a8e97b510e2
nirinchev <irinchev@me.com>
2026-06-09 17:14:18 +0300

    feat: add load sample dataset tool

--- src/tools/atlas/tools.ts
+++ src/tools/atlas/tools.ts
@@ -3,6 +3,7 @@ export { ListProjectsTool } from "./read/listProjects.js";
 export { InspectClusterTool } from "./read/inspectCluster.js";
 export { CreateFreeClusterTool } from "./create/createFreeCluster.js";
 export { CreateClusterTool } from "./create/createCluster.js";
+export { LoadSampleDatasetTool, type LoadSampleDatasetOutput } from "./create/loadSampleDataset.js";
 export { CreateAccessListTool } from "./create/createAccessList.js";
 export { InspectAccessListTool } from "./read/inspectAccessList.js";
 export { ListDBUsersTool } from "./read/listDBUsers.js";
