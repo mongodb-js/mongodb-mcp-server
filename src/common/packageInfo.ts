6fe0baeb87dc3dc8e697d78283bfb1b88cfb2c2c
nirinchev <irinchev@me.com>
2026-09-03 20:08:31 +0200

    Release 2.1.1

--- src/common/packageInfo.ts
+++ src/common/packageInfo.ts
@@ -4,7 +4,7 @@ export const packageInfo: {
     mcpServerName: string;
     engines: { node: string };
 } = {
-    version: "2.1.0",
+    version: "2.1.1",
     mcpServerName: "MongoDB MCP Server",
     engines: {
         node: "^20.19.0 || ^22.13.0 || >= 24.0.0",
